c1775ff1ed18d31bd3d89daa51d59c786f86b8a4
Arkasha <ar246@yandex.ru>
2021-04-08 17:24:36 +0300

    HW2

---
+++ HW2/5.js
@@ -0,0 +1,17 @@
+function sum(a, b) {
+    return (a + b);
+}
+
+function sub(a, b) {
+    return (a - b);
+}
+
+function mul(a, b) {
+    return (a * b);
+}
+
+function div(a, b) {
+    return (a / b);
+}
+
+console.log(sub(1, 3))
